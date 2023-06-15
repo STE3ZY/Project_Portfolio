8098ab6640e741b7c27e51eecf95a5e59fafcffe
steezy <karadimos.giannis@gmail.com>
2023-06-15 22:32:44 +0300

    minor css adjustments

--- src/pages/certificatess/Certificates.js
+++ src/pages/certificatess/Certificates.js
@@ -74,12 +74,8 @@ function Certificates() {
           {certificateItems}
 
           <div className="slider-controler">
-            <div className="swiper-button-prev slider-arrow">
-              <ion-icon name="arrow-back-outline"></ion-icon>
-            </div>
-            <div className="swiper-button-next slider-arrow">
-              <ion-icon name="arrow-forward-outline"></ion-icon>
-            </div>
+            <div className="swiper-button-prev slider-arrow"></div>
+            <div className="swiper-button-next slider-arrow"></div>
             <div className="swiper-pagination"></div>
           </div>
         </Swiper>
